76cdb5e8751b3ed72bcf301af6a16bd3377569f7
stein <test@q.com>
2016-07-13 18:18:07 +0800

    新增有赞develop的client主要用于用户进行的操作

--- demo/youzan/views.py
+++ demo/youzan/views.py
@@ -6,14 +6,15 @@ from django.shortcuts import redirect
 client = YouZanClient('ee709b850e953e912b', '10bbc0de502b27a4cba5df65d33e2d20',
                       'http://youzan.tunnel.phpor.me/youzan/')
 
+
 def home(request):
     data = {'state': 'just for test'}
     url = client.get_authorize_url(data)
     return redirect(url)
 
+
 def callback(request):
     code = request.GET.get('code')
     access_token, error = client.get_access_token(code)
     print access_token
-    res = client.get_resource("kdt.items.onsale.get")
-    return HttpResponse(res)
+    return HttpResponse(access_token)
